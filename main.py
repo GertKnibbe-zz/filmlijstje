140dd84502b96528c49d83216b7115bbc3b33ee7
GertKnibbe-zz <gertknibbe03@gmail.com>
2025-05-25 17:38:13 +0200

    Improve menu structure code

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
-from menus.search import run_search_menu
-from utils import clear_console
+from utils import build_menu
+from menus.search import search_menu
 
 def main_menu() :
     while True :
-        menu ="""
-Welkom bij jouw filmlijstje, kies uit onderstaande:
-        
-    1) Film of serie zoeken op internet
-    2) Bekijk/bewerk watchlist
-    3) Sluit af
-        
-Naar welk menu wil je navigeren?  """
+        choice = build_menu(
+            "Welkom bij jouw filmlijstje, kies uit onderstaande:",
+            [
+                "Film of serie zoeken op internet",
+                "Bekijk/bewerk watchlist",
+                "Sluit af"
+            ]
+        )
 
-        user_input = input(menu)
-        if user_input == "1" :
-            clear_console()
-            run_search_menu()
-        elif user_input == "3" :
+        if choice == "1" :
+            status = search_menu()
+            if status == "back_to_main":
+                continue
+        elif choice == "2" :
+            status = search_menu()
+            if status == "back_to_main":
+                continue
+
+        elif choice == "3" :
             print("App afgesloten")
             break
         else :
             print("Ongeldige input, probeer opnieuw")
-
-
 
 if __name__ == "__main__":
     main_menu()
--- utils.py
+++ utils.py
@@ -12,3 +12,12 @@ def clear_console():
                 print("\n" * 100)
     except Exception:
         print("\n" * 100)
+
+def build_menu(title, options) :
+    print(title)
+    print("---" * 10)
+    for i, option in enumerate(options, start=1):
+        print(f"  {i}) {option}")
+    print("---" * 10)
+    choice_user = input("Wat wil je doen? ")
+    return choice_user
